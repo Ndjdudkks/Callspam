393fb44b0e86802e616b838a9bac8cb4a4f0b4d6
Ndjdudkks <123732505+Ndjdudkks@users.noreply.github.com>
2023-08-27 15:32:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+import asyncio, os
+
+from pyrogram import Client, filters, types, compose
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import AudioPiped, VideoPiped
+from config import CHAT_LINK, API_ID, API_HASH, SESSION_STRING
+from ses import sessions
+
+app = Client(
+    "basic",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING
+)
+
+apps: list["Client"] = []
+calls: list["PyTgCalls"] = []
+
+for a in sessions:
+    apps.append(
+        Client(
+            a["Phone number"],
+            api_id=API_ID,
+            api_hash=API_HASH,
+            session_string=a["Token"],
+            no_updates=True
+        )
+    )
+
+@app.on_message(filters.regex("^join ") & filters.me & filters.reply)
+async def joinChat(c: Client, m: types.Message):
+    chat = m.text.split()[1].replace("@", "")
+    chat_id = (await apps[-1].join_chat(chat)).id
+    await m.edit("Wait ...")
+    if m.reply_to_message.video:
+        Type = VideoPiped
+    elif m.reply_to_message.audio or m.reply_to_message.voice:
+        Type = AudioPiped
+    else:
+        return await m.edit("rep to audio voice video")
+    await m.edit("Downloading ....")
+    path = await m.reply_to_message.download()
+    await m.edit("Joining ..")
+    for a in apps:
+        try:
+            await a.join_chat(chat)
+        except:
+            pass
+    count = 0
+    for call in calls:
+        try:
+            await call.join_group_call(
+                chat_id=chat_id,
+                stream=Type(path=path)
+            )
+            count += 1
+        except:
+            pass
+    return await apps[0].send_message(m.chat.id, f"Successfully Joined to {chat_id}", reply_to_message_id=m.id)
+
+@app.on_message(filters.regex("^leave ") & filters.me)
+async def leaveChat(c: Client, m: types.Message):
+    chat = m.text.split()[1].replace("@", "")
+    chat_id = (await apps[-1].join_chat(chat)).id
+    for a in apps:
+        try:
+            await a.leave_chat(chat_id=chat_id)
+        except:
+            pass
+    return await m.edit("Done")
+
+async def main():
+    await app.start()
+    await compose(apps)
+    count = 0
+    for a in apps:
+        call = PyTgCalls(a)
+        try:
+            await call.start()
+            calls.append(call)
+            await a.join_chat(CHAT_LINK)
+            count += 1
+        except:
+            pass
+    print(f"Started {count} session")
+    await idle()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ ses.py
@@ -0,0 +1,20 @@
+sessions = [
+    {
+        "Name": "¹",
+        "Phone number": "18137219766",
+        "ID": 5613324628,
+        "Token": "AQAZ0RIAnRzFSqyZy9HM9b2kgPd1Zia1_aRmM633vyHS0KFIwh6ugJgd2q0sWPsg8I4HPeeyJic74blUQ5R6D3rczslDQzvjpUYWMzaGUIcQZC2aiJV-wKnbrgkpp0TZY-c75xAl8_alVVF0BY_BoChPK139Db3tlVsj4r5tUI69X_JmJCe7KDj2rSDkzyiDD7axFtg4d3OXRZfnAx6IWsVAcUuvzTUXF96ojFANs9CM1eUkB1DMEJZP61_HUvlPiimzB7j4UNYEtCX2SPOif2-P0pt4FsocrGArB0sEbzVU1bR5WbOCpjWIA1B8xL-hU4QbwxwPBnXFPXavIfr-H_KeeuLCIAAAAAFOlIlUAA"
+    },
+    {
+        "Name": "²",
+        "Phone number": "15185546243",
+        "ID": 5795912596,
+        "Token": "AQAZ0RIAGkoo6zJrB6OBi2yAlIzGNnnx55Y4YDAZya3EVcNROzO9PjLWuvLfhfl6hs_irABSExGTFrFQivyfTw6hY8TJyoskETMh3HlHEjtua5nvbaVIzDGb1gBr9Rb6Xe6BFAXHQmqnGgJjFkw63f9dCsHssr4bmKcyLs1uKduvMQYqgAbu712VrLRUUi__Bcohx0xPFw3-0WPBjuhG8aZCRccsLxH1pur5mdZbsLvIIgSl73niX-smbhxJbK7X9JSa44JIbg-6Zt6_C9yAmgkojxIW2cDnOt5K21QvvxbeWf9qlPW8G9tAjaTXlg2eXQOfbu-62h104HK9n2j5pBtpXdOAYgAAAAFZdpuUAA"
+    },
+    {
+        "Name": "³",
+        "Phone number": "18139121696",
+        "ID": 5640604058,
+        "Token": "AQAZ0RIAZ4X_pBSapQ42TMZFDcjccttffXU7Z6I3rGYsOFlXAvjFxFpYWYnzPFdTJTAF_zOf-A1BNNAjbJr6Aorn2qEQIAfPqY8AY-xeC9YiUs7CVIG6JzrNmd46J_VKT-4RLzRfis0YoPWh2ImMicrgFGX3FDMIK5fQRpVSE3bDUjpKH4hI6acBnhoWiWpsf0vKfI5XZdp3vFcFV6ISWE7Ro8fBX33BLdb_tOKzREbA6CrqEePRwPGCGeLsyzxhnWgrt6jGLllrRMKLXUBoizFJ2sKSmj98VtW7nq07vKAgeHFXv_JW5JPStGGO4e98UJA-FzUAMmZilOtJDi0CtKF2iAEirgAAAAFQNMmaAA"
+    }
+]
