949ea07c2f1ee70fd722bca5ccffefa492f3e6b7
Ndjdudkks <123732505+Ndjdudkks@users.noreply.github.com>
2023-08-28 12:21:35 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-CHAT_LINK = os.getenv('CHAT_LINK', None)
-API_ID = int(os.getenv("API_ID", "13251350"))
-API_HASH = os.getenv("API_HASH", "66c0eacb36f9979ae6d153f207565cd6")
-SESSION_STRING = os.getenv("SESSION_STRING", "session string here")
